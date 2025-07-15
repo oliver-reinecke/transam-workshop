9ea8b41f52580f610b0bf2ae005811a6abf54daf
oliver-reinecke <oliver.reinecke@dataiku.com>
2025-07-15 11:47:54 -0600

    Update myfunctions.py

--- myfunctions.py
+++ myfunctions.py
@@ -1,6 +1,9 @@
 def check_for_alert(change_vs_avg, alert_threshold):
-"""Checks if a percentage change indicates a significant dip, triggering an alert.Returns 1 if an alert is triggered, 0 otherwise."""
-if change_vs_avg < alert_threshold:
-return 1
-else:
-return 0
+    """
+    Checks if a percentage change indicates a significant dip, triggering an alert.
+    Returns 1 if an alert is triggered, 0 otherwise.
+    """
+    if change_vs_avg < alert_threshold:
+        return 1
+    else:
+        return 0
